0429901d3d40902e1a1c19dd403c83289ef9ae6f
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2019-04-02 15:18:03 +0200

    locationDataTrans app is added

---
+++ apps/locationDataTrans/sensorComp/main.c
@@ -0,0 +1,46 @@
+#include "legato.h"
+#include "interfaces.h"
+
+COMPONENT_INIT
+{
+
+  int32_t latitude;
+  int32_t longitude;
+  int32_t horizontalAccuracy;
+
+  int32_t *latitudePtr = &latitude;
+  int32_t *longitudePtr = &longitude;
+  int32_t *horizontalAccuracyPtr = &horizontalAccuracy;
+
+  LE_INFO ("ERORR 1");
+
+  le_pos_Get2DLocation(latitudePtr, longitudePtr, horizontalAccuracyPtr);
+
+  char latitudeString[100];
+  char longitudeString[100];
+
+  sprintf(latitudeString,"%d", *latitudePtr);
+  sprintf(longitudeString,"%d", *longitudePtr);
+
+  LE_INFO ("ERORR 2");
+
+  char destination[] = "0695162402";
+  char text[1000];
+  strcpy(text , "Hello. This message is from MangOH.");
+  strcat(text, "\nYour latitude: ");
+  strcat(text, latitudeString);
+  strcat(text, "\nYour longitude: ");
+  strcat(text, longitudeString);
+
+  LE_INFO ("ERORR 3");
+
+  le_sms_MsgRef_t myMsg = le_sms_Create();
+
+  le_sms_SetDestination(myMsg, destination);
+  le_sms_SetText(myMsg, text);
+  le_sms_Send(myMsg);
+  le_sms_Delete(myMsg);
+
+  LE_INFO ("ERORR 4");
+
+}
